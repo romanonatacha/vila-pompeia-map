53415ee942b305552a4389183e404da685d61cd3
natacha romano <romano.n@hotmail.com>
2018-12-22 19:59:13 -0200

    semi final project

---
+++ src/components/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import scriptLoader from "react-async-script-loader";
+import { MAP_KEY } from "../data/credentials";
+import { mapStyles } from "../data/mapStyles.js";
+import List from "./List";
+import loader from "../images/loader.gif";
+import foursquare from "../images/foursquare.png";
+
+class App extends Component {
+  state = {
+    listOpen: false,
+    map: {},
+    infowindow: {},
+    bounds: {},
+    mapReady: false,
+    // for future use when add location search
+    mapCenter: { lat: -23.5355903, lng: -46.6884227 },
+    mapError: false,
+    width: window.innerWidth
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWidth);
+  }
+
+  componentDidUpdate({ isScriptLoadSucceed }) {
+    // Check if script is loaded and if map is defined
+    if (isScriptLoadSucceed && !this.state.mapReady) {
+      // create map
+      const map = new window.google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: this.state.mapCenter,
+        styles: mapStyles
+      });
+
+      // set up bounds and infowindow to use later
+      const bounds = new window.google.maps.LatLngBounds();
+      const infowindow = new window.google.maps.InfoWindow({ maxWidth: 300 });
+
+      this.setState({
+        map: map,
+        infowindow: infowindow,
+        bounds: bounds,
+        mapReady: true
+      });
+
+      // alert user if map request fails
+    } else if (!this.state.mapReady) {
+      this.setState({ mapError: true });
+    }
+  }
+
+  toggleList = () => {
+    const { width, listOpen, infowindow } = this.state;
+
+    if (width < 600) {
+      // close infowindow if listview is opening
+      if (!listOpen) {
+        infowindow.close();
+      }
+      this.setState({ listOpen: !listOpen });
+    }
+  };
+
+  updateWidth = () => {
+    const { map, bounds } = this.state;
+    this.setState({ width: window.innerWidth });
+    if (map && bounds) {
+      map.fitBounds(bounds);
+    }
+  };
+
+  render() {
+    const {
+      listOpen,
+      map,
+      infowindow,
+      bounds,
+      mapReady,
+      mapCenter,
+      mapError
+    } = this.state;
+
+    return (
+      <div className="container" role="main">
+        <nav id="list-toggle" className="toggle" onClick={this.toggleList}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z" />
+          </svg>
+        </nav>
+        <section
+          id="restaurant-list"
+          className={listOpen ? "list open" : "list"}
+          role="complementary"
+          tabIndex={listOpen ? "0" : "-1"}
+        >
+          <h1 className="app-title">Vila Pompeia<br/>Food n' Bar</h1>
+          
+          {/* render markers only when map has loaded */
+          mapReady ? (
+            <List
+              map={map}
+              infowindow={infowindow}
+              bounds={bounds}
+              mapCenter={mapCenter}
+              toggleList={this.toggleList}
+              listOpen={listOpen}
+            />
+          ) : (
+            <p>
+              We are experiencing loading issues. Please check your internet
+              connection
+            </p>
+          )}
+          <img
+            src={foursquare}
+            alt="Powered by Foursquare"
+            className="foursquare-logo"
+          />
+        </section>
+        <section id="map" className="map" role="application">
+          {mapError ? (
+            <div id="map-error" className="error" role="alert">
+              Oops something wrong... Please come back later...
+            </div>
+          ) : (
+            <div className="loading-map">
+              <h4 className="loading-text">Loading...</h4>
+              <img src={loader} className="loader" alt="loading" />
+            </div>
+          )}
+        </section>
+      </div>
+    );
+  }
+}
+
+export default scriptLoader([
+  `https://maps.googleapis.com/maps/api/js?key=${MAP_KEY}`
+])(App);
---
+++ src/components/List.js
@@ -0,0 +1,167 @@
+import React, { Component } from "react";
+import Place from "./Place";
+import { getFSLocations, getFSDetails } from "../apis/foursquare";
+import { checkData, buildInfoContent, buildErrorContent } from "../utils/helpers";
+import markerIcon from "../images/marker.png";
+import loader from "../images/loader.gif";
+import PropTypes from "prop-types";
+
+class List extends Component {
+  static propTypes = {
+    map: PropTypes.object.isRequired,
+    infowindow: PropTypes.object.isRequired,
+    bounds: PropTypes.object.isRequired,
+    mapCenter: PropTypes.object.isRequired,
+    toggleList: PropTypes.func.isRequired,
+    listOpen: PropTypes.bool.isRequired
+  };
+
+  state = {
+    query: "",
+    allPlaces: [],
+    filteredPlaces: null,
+    apiReturned: false
+  };
+
+  componentDidMount() {
+    getFSLocations(this.props.mapCenter)
+      .then(places => {
+        this.setState({
+          allPlaces: places,
+          filteredPlaces: places,
+          apiReturned: true
+        });
+        if (places) this.addMarkers(places);
+      })
+      .catch(error => this.setState({ apiReturned: false }));
+  }
+
+  addMarkers(places) {
+    const { map, bounds, infowindow, toggleList } = this.props;
+    const self = this;
+
+    places.forEach(location => {
+      const position = {
+        lat: location.location.lat,
+        lng: location.location.lng
+      };
+
+      location.marker = new window.google.maps.Marker({
+        position,
+        map,
+        title: location.name,
+        id: location.id,
+        icon: markerIcon
+      });
+
+      bounds.extend(position);
+
+      location.marker.addListener("click", function() {
+        const marker = this;
+
+        // bounce marker three times then stop
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        setTimeout(() => marker.setAnimation(null), 2100);
+
+        // get venue details and display in infowindow
+        getFSDetails(marker.id)
+          .then(data => {
+            checkData(marker, data);
+            buildInfoContent(marker);
+          })
+          .catch(() => buildErrorContent(marker))
+          .finally(() => {
+            // set content and open window
+            infowindow.setContent(marker.infoContent);
+            infowindow.open(map, marker);
+
+            // close list view in mobile if open so infowindow is not hidden by list
+            if (self.props.listOpen) {
+              toggleList();
+            }
+          });
+      });
+    });
+
+    // size and center map
+    map.fitBounds(bounds);
+  }
+
+  filterPlaces = event => {
+    const { allPlaces } = this.state;
+    const { infowindow } = this.props;
+    const query = event.target.value.toLowerCase();
+
+    // update state so input box shows current query value
+    this.setState({ query: query });
+
+    // close infoWindow when filter runs
+    infowindow.close();
+
+    // filter list markers by name of location
+    const filteredPlaces = allPlaces.filter(place => {
+      const match = place.name.toLowerCase().indexOf(query) > -1;
+      place.marker.setVisible(match);
+      return match;
+    });
+
+    // sort array before updating state
+    filteredPlaces.sort(this.sortName);
+
+    this.setState({ filteredPlaces: filteredPlaces });
+  };
+
+  showInfo = place => {
+    // force marker click
+    window.google.maps.event.trigger(place.marker, "click");
+  };
+
+  render() {
+    const { apiReturned, filteredPlaces, query } = this.state;
+    const { listOpen } = this.props;
+
+    // API request fails
+    if (apiReturned && !filteredPlaces) {
+      return <div> Oops, something is wrong! Please come back later.</div>;
+
+      // API request returns successfully
+    } else if (apiReturned && filteredPlaces) {
+      return (
+        <div>
+          <input
+            type="text"
+            placeholder="find a place to eat or drink"
+            value={query}
+            onChange={this.filterPlaces}
+            className="query"
+            role="search"
+            aria-labelledby="text filter"
+            tabIndex={listOpen ? "0" : "-1"}
+          />
+          {apiReturned && filteredPlaces.length > 0 ? (
+            <ul className="list-places">
+              {filteredPlaces.map((place, id) => (
+                <Place key={place.id} place={place} listOpen={listOpen} />
+              ))}
+            </ul>
+          ) : (
+            <p id="filter-error" className="no-results">
+              No results :(
+            </p>
+          )}
+        </div>
+      );
+
+      // API request has not returned yet
+    } else {
+      return (
+        <div className="loading-fs">
+          <h4 className="loading-text">Loading Restaurants...</h4>
+          <img src={loader} className="loader" alt="loading" />
+        </div>
+      );
+    }
+  }
+}
+
+export default List;
