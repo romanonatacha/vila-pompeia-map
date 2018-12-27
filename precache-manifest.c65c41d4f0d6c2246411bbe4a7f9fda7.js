67585327925e8f587eacf1cb1565708ad94203a4
natacha romano <romano.n@hotmail.com>
2018-12-27 08:18:33 -0200

    Updates

---
+++ precache-manifest.c65c41d4f0d6c2246411bbe4a7f9fda7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6863045f2ab2f811054ff3143412777c",
+    "url": "/vila-pompeia-map/static/media/loader.6863045f.gif"
+  },
+  {
+    "revision": "e7dda5140f3cd2f2fe545fe6affc9faa",
+    "url": "/vila-pompeia-map/static/media/foursquare.e7dda514.png"
+  },
+  {
+    "revision": "2dbc3076a8201d7bc7ae",
+    "url": "/vila-pompeia-map/static/js/runtime~main.2dbc3076.js"
+  },
+  {
+    "revision": "3dab3ef4304c8504f549",
+    "url": "/vila-pompeia-map/static/js/main.3dab3ef4.chunk.js"
+  },
+  {
+    "revision": "67997e5585b550a806cb",
+    "url": "/vila-pompeia-map/static/js/1.67997e55.chunk.js"
+  },
+  {
+    "revision": "3dab3ef4304c8504f549",
+    "url": "/vila-pompeia-map/static/css/main.cc7566ec.chunk.css"
+  },
+  {
+    "revision": "b7975914f7fd92a79c3ed6644162f454",
+    "url": "/vila-pompeia-map/index.html"
+  }
+];
